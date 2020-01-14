74284a9f5d0ab0ebd45295a730ab704282799a4c
ysd <ysd@emomo.com>
2020-01-14 18:00:21 +0800

    [dev_ysd]add

--- host/pro/settings/24l01.Debug.cspy.bat
+++ host/pro/settings/24l01.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"D:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "F:\24l01\host\pro\settings\24l01.Debug.general.xcl" --backend -f "F:\24l01\host\pro\settings\24l01.Debug.driver.xcl" 
+"D:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "C:\Users\Administrator\Desktop\24l01\host\pro\settings\24l01.Debug.general.xcl" --backend -f "C:\Users\Administrator\Desktop\24l01\host\pro\settings\24l01.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"D:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "F:\24l01\host\pro\settings\24l01.Debug.general.xcl" "--debug_file=%~1" --backend -f "F:\24l01\host\pro\settings\24l01.Debug.driver.xcl" 
+"D:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "C:\Users\Administrator\Desktop\24l01\host\pro\settings\24l01.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Administrator\Desktop\24l01\host\pro\settings\24l01.Debug.driver.xcl" 
 
 @echo off 
 :end
